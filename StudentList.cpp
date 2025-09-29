6a188977899f2ce02c130ab4d4d1317da60b1034
U-BSD\497022 <497022@SHS-8YBF6X3.beaverton.k12.or.us>
2025-09-29 09:54:27 -0700

    Added remove and average functions, optimized rounding.

--- StudentList.cpp
+++ StudentList.cpp
@@ -5,15 +5,15 @@
   This program serves as a database for students. The user may add and remove students, as
   well as see the complete list of students they've entered. Each student has a first and last
   name, as well as an ID number and a GPA (0.0-5.0).
-  git 
+
   Author: Luca Ardanaz
   Last Updated: 9/26/2025
  */
 
 #include <iostream>
 #include <cstring>
-#include <vector>
 #include <cmath>
+#include <bits/stdc++.h>
 #include "Node.h"
 #include "Student.h"
 
@@ -21,10 +21,12 @@ using namespace std;
 
 void addStudent(Node* &head);
 void printStudents(Node* current, Node* &head);
-void removeStudent(vector<Student*> &s);
+void removeStudent(Node* &head);
 void cinReset();
 
 void insertStudentByID(Node* s, Node* previous, Node* current, Node* &head);
+void removeStudentByID(int ID, Node* previous, Node* current, Node* &head);
+void printAverageGPA(double sumX, double n, Node* current, Node* &head);
 
 int main() {
 
@@ -47,6 +49,7 @@ int main() {
       cout << " - ADD: Add a new student to the database." << endl;
       cout << " - PRINT: Prints out all of the students in the database." << endl;
       cout << " - DELETE: Remove a student from the database." << endl;
+      cout << " - AVERAGE: Calculates the average GPA of all the students." << endl;
       cout << " - QUIT: Close the program" << endl;
     }
     else if (strcmp(input, "ADD") == 0) {
@@ -59,7 +62,11 @@ int main() {
     }
     else if (strcmp(input, "DELETE") == 0) {
       //remove a student
-      removeStudent(students);
+      removeStudent(head);
+    }
+    else if (strcmp(input, "AVERAGE") == 0) {
+      //calculate average GPA
+      printAverageGPA(0, 0, head, head);
     }
     
   } while (strcmp(input, "QUIT") != 0);
@@ -75,7 +82,7 @@ void addStudent(Node* &head) {
   char* firstName = new char[20];
   char* lastName = new char[20];
   int ID = 0;
-  int GPA = 0;
+  double GPA = 0;
   
   //asks for first name
   cout <<"Adding a new student..." << endl;
@@ -122,7 +129,6 @@ void addStudent(Node* &head) {
 void insertStudentByID(Node* s, Node* previous, Node* current, Node* &head) {
   //if there is no head, make it the new head
   if (head == NULL) {
-    cout << "-Setting new head" << endl;
     head = s;
     return;
   }
@@ -133,12 +139,10 @@ void insertStudentByID(Node* s, Node* previous, Node* current, Node* &head) {
     //go to the next element if there is one
 
     if (current -> getNext() != NULL) {
-      cout << "Going deeper..." << endl;
       insertStudentByID(s, current, current -> getNext(), head);
     }
     else {
       //there is no list left, just add it to the end
-      cout << "-Setting new tail" << endl;
       current -> setNext(s);  
     }
   }
@@ -147,13 +151,11 @@ void insertStudentByID(Node* s, Node* previous, Node* current, Node* &head) {
 
     //if there is no previous, assign it as the new head and bump everything back
     if (previous == NULL) {
-      cout << "-Bumping head" << endl;
       head = s;
       s -> setNext(current);
     }
     //link the previous to it and it to the current
     else {
-      cout << "-Setting new body" << endl;
       previous -> setNext(s);
       s -> setNext(current);
     }
@@ -164,6 +166,11 @@ void insertStudentByID(Node* s, Node* previous, Node* current, Node* &head) {
 }
 
 void printStudents(Node* current, Node* &head) {
+  //if there aren't any students, tell the user
+  if (head == NULL) {
+    cout << "There aren't any students yet" << endl;
+    return;
+  }
   
   if (current == head) {
     //means its the first item of the list
@@ -175,13 +182,21 @@ void printStudents(Node* current, Node* &head) {
       The confusing looking logic with the modulos fixes the issue where numbers like 4.00 are displayed as 4.
       Essentially it just adds the ".00" to 4 or similarly a "0" to 3.2. That way the precision always shows the same.
     */
-    cout << current -> getStudent() -> getFirstName() << " " << current -> getStudent() -> getLastName() << ", " << current -> getStudent() -> getID() << ", " << round((current -> getStudent() -> getGPA()) * 100) / 100 << (int(round((current -> getStudent() -> getGPA()) * 100)) % 10 == 0 ? (int(round((current -> getStudent() -> getGPA()) * 100)) % 100 == 0 ? ".00" : "0") : "") << endl;
+    cout << current -> getStudent() -> getFirstName() << " " << current -> getStudent() -> getLastName() << ", " << current -> getStudent() -> getID() << ", ";
+    cout << fixed << setprecision(2) << current -> getStudent() -> getGPA() << endl;
 
     printStudents(current -> getNext(), head);
   }
 }
 
-void removeStudent(vector<Student*> &s) {
+void removeStudent(Node* &head) {
+
+  //if the linked list is empty, tell the user and exit
+  if (head == NULL) {
+    cout << "There aren't any students yet." << endl;
+    return;
+  }
+  
   //asks for ID and saves it
   //keeps asking till it gets an int
   int id = 0;
@@ -195,15 +210,74 @@ void removeStudent(vector<Student*> &s) {
 
   cinReset();
 
-  //removes ALL students who have that ID
-  for (int i = 0; i < s.size(); i++) {
-    if ((*(s.at(i))).getID() == id) {
-      //deletes student and gets rid of pointer
-      delete s.at(i);
-      s.erase(s.begin() + i);
-      i--;
-    }
+  removeStudentByID(id, NULL, head, head);
+}
+
+void removeStudentByID(int ID, Node* previous, Node* current, Node* &head) {
+  //if the list is empty, return
+  if (head == NULL) {
+    return;
   }
+
+  //checks if the current node matches
+  if (current -> getStudent() -> getID() == ID) {
+    //head deletion case
+    if (current == head) {
+      head = current -> getNext();
+    }
+    
+    //tail deletion case
+    else if (current -> getNext() == NULL) {
+      previous -> setNext(NULL);
+    }
+    
+    //body deletion case
+    else {
+      previous -> setNext(current -> getNext());
+    }
+
+    delete current;
+    return;
+  }
+
+  //if the node isn't the last one, continue search
+  if (current -> getNext() != NULL) {
+    removeStudentByID(ID, current, current -> getNext(), head);
+  }
+
+  
+}
+
+void printAverageGPA(double sumX, double n, Node* current, Node* &head) {
+  //if the list is empty, tell the user
+  if (head == NULL) {
+    cout << "There aren't any students" << endl;
+    return;
+  }
+
+  //if this is the first node, make sure that sum and n are both reset
+  if (current == head) {
+    sumX = 0;
+    n = 0;
+  }
+
+  //add the gpa to sumX and add 1 to n to track the number of students
+  sumX += current -> getStudent() -> getGPA();
+  n++;
+
+  //checks if this is the last student
+  if (current -> getNext() == NULL) {
+    //print out the average and finish
+    double average = sumX / n;
+    cout << average;
+    cout << "Average GPA: ";
+    cout << fixed << setprecision(2) << average << endl;
+  }
+  else {
+    //go to the next student
+    printAverageGPA(sumX, n, current -> getNext(), head);
+  }
+  
 }
 
 //this clears all errors with cin so that cin always works as expected.
